5c497d95e76cff61604e2a6b0326810fa5b82eeb
StefanGary <stefanfgary@gmail.com>
2023-11-16 18:25:35 +0000

    system call to timeout does not help either
    Defunct processes are zombies; they have already died. That they
    remain means that they are not being properly monitored by the
    parent process (in this case, we may need to look into subprocess.run())

--- data_provider/rsync.py
+++ data_provider/rsync.py
@@ -10,8 +10,7 @@ def get_stage_in_cmd(file, jumphost = None):
         # does not normally work in bash, but works for
         # the special case we use subprocess.run(cmd, shell=True)
         # in the wrapper.
-        #cmd = "ssh -J {jumphost} {hostname} exit; rsync -avzq  -e 'ssh -J {jumphost}' {hostname}:{permanent_filepath} {worker_filepath}".format(
-        cmd = "timeout 60 rsync -avzq -e 'ssh -J {jumphost}' {hostname}:{permanent_filepath} {worker_filepath}".format(
+        cmd = "ssh -J {jumphost} {hostname} exit; rsync -avzq  -e 'ssh -J {jumphost}' {hostname}:{permanent_filepath} {worker_filepath}".format(
             jumphost = jumphost,
             hostname = file.netloc,
             permanent_filepath = file.path,
@@ -32,8 +31,7 @@ def get_stage_out_cmd(file, jumphost = None):
         # does not normally work in bash, but works for
         # the special case we use subprocess.run(cmd, shell=True)
         # in the wrapper.
-        #cmd = "ssh -J {jumphost} {hostname} exit; rsync -avzq -e 'ssh -J {jumphost}' --rsync-path=\"mkdir -p {root_path} && rsync\" {worker_filepath} {hostname}:{permanent_filepath}".format(
-        cmd = "timeout 60 rsync -avzq -e 'ssh -J {jumphost}' --rsync-path=\"mkdir -p {root_path} && rsync\" {worker_filepath} {hostname}:{permanent_filepath}".format(
+        cmd = "ssh -J {jumphost} {hostname} exit; rsync -avzq -e 'ssh -J {jumphost}' --rsync-path=\"mkdir -p {root_path} && rsync\" {worker_filepath} {hostname}:{permanent_filepath}".format(
             jumphost = jumphost,
             hostname = file.netloc,
             permanent_filepath = file.path,
